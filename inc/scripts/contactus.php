93226345126219a84583fbf4f0817df866953e11
williamhaynes <w.haynes@rgu.ac.uk>
2016-10-31 14:45:00 +0000

    moved style to correct directory

--- inc/scripts/contactus.php
+++ inc/scripts/contactus.php
@@ -1,10 +1,8 @@
 <?php
-include ("scripts/header.php");
-echo "
-<main>
+    include ("scripts/header.php");
+    echo "<main>
 <h2>Contact</h2>
 <p>Send me an email at babbage@babbage-labs.com</p>
-</main>
-";
-include ("scripts/footer.php");
+</main>";
+    include ("scripts/footer.php");
 ?>
